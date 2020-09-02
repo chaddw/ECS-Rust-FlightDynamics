65fb52a28e10ec9a40b41621c0a32e682eee4b8b
chaddw <caw14f@my.fsu.edu>
2020-09-02 12:28:13 -0400

    cleaned up code, fixed snake case

--- net_fdm/src/main.rs
+++ net_fdm/src/main.rs
@@ -1,130 +1,99 @@
 
 use std::net::UdpSocket;
 use std::{thread, time};
-//use socket::{AF_INET, SO_REUSEADDR, SOCK_DGRAM, Socket, SOL_SOCKET};
-//use std::time::{Duration, SystemTime};
-//use socket::htonl;
-//use serde::ser::{Serialize, Serializer, SerializeStruct};
-//use serde::{Deserialize, Serialize};
+
+//FlightGear is ran with this line of command argumments on the fgfs executable:
+//fgfs.exe --aircraft=ufo --disable-panel --disable-sound --enable-hud --disable-random-objects --fdm=null --vc=0 --timeofday=noon --native-fdm=socket,in,30,,5500,udp
 
 
-
-//#[allow(unused_imports)]
-//extern crate byteorder;
-//use byteorder::{ByteOrder, NetworkEndian, BigEndian, LittleEndian, WriteBytesExt};
-//use std::mem::transmute;
-//use std::mem;
-
-
-
-// enum stuff {
-//     FG_MAX_ENGINES,
-//     FG_MAX_WHEELS,
-//     FG_MAX_TANKS,
-// }//{FG_MAX_ENGINES:: 4, FG_MAX_WHEELS:: 3, FG_MAX_TANKS:: 4}
-
-
-//#[derive(Serialize, Deserialize, Debug)]
 #[derive(Default)]
-#[repr(C)] //fixed padding issue
-#[allow(non_snake_case)]
+#[repr(C)] //fix padding issue
 struct FGNetFDM
-{ //recreate the structure
-
-    //enumOfStuff: stuff(FG_MAX_ENGINES: 4, FG_MAX_WHEELS: 3, FG_MAX_TANKS: 4),
-
-
-    version: u32, // uint32_t version;		// increment when data values change
-    padding: f32, // uint32_t padding;		// padding
+{ 
+    version: u32, // increment when data values change
+    padding: f32, // padding
 
     // // Positions
-    longitude: f64, // double longitude;		// geodetic (radians)
-    latitude: f64, // double latitude;		// geodetic (radians)
-    altitude: f64, // double altitude;		// above sea level (meters)
-    agl: f32, // float agl;			// above ground level (meters)
-    phi: f32, // float phi;			// roll (radians)
-    theta: f32, // float theta;		// pitch (radians)
-    psi: f32, // float psi;			// yaw or true heading (radians)
-    alpha: f32, // float alpha;                // angle of attack (radians)
-    beta: f32, // float beta;                 // side slip angle (radians)
+    longitude: f64, // geodetic (radians)
+    latitude: f64, // geodetic (radians)
+    altitude: f64, // above sea level (meters)
+    agl: f32, // above ground level (meters)
+    phi: f32, // roll (radians)
+    theta: f32, // pitch (radians)
+    psi: f32, // yaw or true heading (radians)
+    alpha: f32, // angle of attack (radians)
+    beta: f32, // side slip angle (radians)
 
     // // Velocities
-    phidot: f32, // float phidot;		// roll rate (radians/sec)
-    thetadot: f32, // float thetadot;		// pitch rate (radians/sec)
-    psidot: f32, // float psidot;		// yaw rate (radians/sec)
-    vcas: f32, // float vcas;		        // calibrated airspeed
-    climb_rate: f32, // float climb_rate;		// feet per second
-    v_north: f32, // float v_north;              // north velocity in local/body frame, fps
-    v_east: f32, // float v_east;               // east velocity in local/body frame, fps
-    v_down: f32, // float v_down;               // down/vertical velocity in local/body frame, fps
-    v_body_u: f32, // float v_body_u;    // ECEF velocity in body frame
-    v_body_v: f32, // float v_body_v;    // ECEF velocity in body frame 
-    v_body_w: f32, // float v_body_w;    // ECEF velocity in body frame
+    phidot: f32, // roll rate (radians/sec)
+    thetadot: f32, // pitch rate (radians/sec)
+    psidot: f32, // yaw rate (radians/sec)
+    vcas: f32, // calibrated airspeed
+    climb_rate: f32, // feet per second
+    v_north: f32, // north velocity in local/body frame, fps
+    v_east: f32, // east velocity in local/body frame, fps
+    v_down: f32, // down/vertical velocity in local/body frame, fps
+    v_body_u: f32, // ECEF velocity in body frame
+    v_body_v: f32, // ECEF velocity in body frame 
+    v_body_w: f32, // ECEF velocity in body frame
     
     // // Accelerations
-    A_X_pilot: f32, // float A_X_pilot;		// X accel in body frame ft/sec^2
-    A_y_pilot: f32, // float A_Y_pilot;		// Y accel in body frame ft/sec^2
-    A_Z_pilot: f32,// float A_Z_pilot;		// Z accel in body frame ft/sec^2
+    a_x_pilot: f32, // X accel in body frame ft/sec^2
+    a_y_pilot: f32, // Y accel in body frame ft/sec^2
+    a_z_pilot: f32, // Z accel in body frame ft/sec^2
 
     // // Stall
-    stall_warning: f32,// float stall_warning;        // 0.0 - 1.0 indicating the amount of stall
-    slip_deg: f32, // float slip_deg;		// slip ball deflection
-
-    // // Pressure
+    stall_warning: f32, // 0.0 - 1.0 indicating the amount of stall
+    slip_deg: f32, // slip ball deflection
     
     // // Engine status
-    num_engines: u32, // uint32_t num_engines;	     // Number of valid engines
-    eng_state: [f32; 4], // uint32_t eng_state[FG_MAX_ENGINES];// Engine state (off, cranking, running)
-    rpm: [f32; 4], // float rpm[FG_MAX_ENGINES];	     // Engine RPM rev/min
-    fuel_flow: [f32; 4], // float fuel_flow[FG_MAX_ENGINES]; // Fuel flow gallons/hr
-    fuel_px: [f32; 4], // float fuel_px[FG_MAX_ENGINES];   // Fuel pressure psi
-    egt: [f32; 4], // float egt[FG_MAX_ENGINES];	     // Exhuast gas temp deg F
-    cht: [f32; 4], // float cht[FG_MAX_ENGINES];	     // Cylinder head temp deg F
-    mp_osi: [f32; 4], // float mp_osi[FG_MAX_ENGINES];    // Manifold pressure
-    tit: [f32; 4], // float tit[FG_MAX_ENGINES];	     // Turbine Inlet Temperature
-    oil_temp: [f32; 4], // float oil_temp[FG_MAX_ENGINES];  // Oil temp deg F
-    oil_px: [f32; 4], // float oil_px[FG_MAX_ENGINES];    // Oil pressure psi
+    num_engines: u32, // Number of valid engines
+    eng_state: [f32; 4], // Engine state (off, cranking, running)
+    rpm: [f32; 4], // // Engine RPM rev/min
+    fuel_flow: [f32; 4], // Fuel flow gallons/hr
+    fuel_px: [f32; 4], // Fuel pressure psi
+    egt: [f32; 4], // Exhuast gas temp deg F
+    cht: [f32; 4], // Cylinder head temp deg F
+    mp_osi: [f32; 4], // Manifold pressure
+    tit: [f32; 4], // Turbine Inlet Temperature
+    oil_temp: [f32; 4], // Oil temp deg F
+    oil_px: [f32; 4], // Oil pressure psi
 
     // // Consumables
-    num_tanks: u32, // uint32_t num_tanks;		// Max number of fuel tanks
-    fuel_quantity: [f32; 4], // float fuel_quantity[FG_MAX_TANKS];
+    num_tanks: u32, // Max number of fuel tanks
+    fuel_quantity: [f32; 4], 
 
     // // Gear status
-    num_wheels: u32, // uint32_t num_wheels;
-    wow: [f32; 3], // uint32_t wow[FG_MAX_WHEELS];
-    gear_pos: [f32; 3], // float gear_pos[FG_MAX_WHEELS];
-    gear_steer: [f32; 3],// float gear_steer[FG_MAX_WHEELS];
-    gear_compression: [f32; 3],// float gear_compression[FG_MAX_WHEELS];
+    num_wheels: u32, 
+    wow: [f32; 3], 
+    gear_pos: [f32; 3],
+    gear_steer: [f32; 3],
+    gear_compression: [f32; 3],
 
     // // Environment
-    cur_time: f32, // uint32_t cur_time;           // current unix time
-    //                                 // FIXME: make this uint64_t before 2038
-    warp: f32, // int32_t warp;                // offset in seconds to unix time
-    visibility: f32, // float visibility;            // visibility in meters (for env. effects)
+    cur_time: f32, // current unix time
+    warp: f32, // offset in seconds to unix time
+    visibility: f32, // visibility in meters (for env. effects)
 
     // // Control surface positions (normalized values)
-    elevator: f32, // float elevator;
-    elevator_trim_tab: f32, // float elevator_trim_tab;
-    left_flap: f32, // float left_flap;
-    right_flap: f32, // float right_flap;
-    left_aileron: f32, // float left_aileron;
-    right_aileron: f32, // float right_aileron;
-    rudder:f32, // float rudder;
-    nose_wheel: f32,// float nose_wheel;
-    speedbrake: f32,// float speedbrake;
-    spoilers: f32// float spoilers;
-
+    elevator: f32,
+    elevator_trim_tab: f32, 
+    left_flap: f32,
+    right_flap: f32,
+    left_aileron: f32, 
+    right_aileron: f32, 
+    rudder: f32, 
+    nose_wheel: f32,
+    speedbrake: f32,
+    spoilers: f32
 }
 
-    
-
-#[allow(non_snake_case)]
 fn main()
 {
 
-    let D2R = (3.14159 / 180.0) as f64; //should be float... htonl wants u32
-    //println!("{}", D2R);
-    let FG_NET_FDM_VERSION = 24_u32;
+    let d2r = (3.14159 / 180.0) as f64; 
+
+    let fg_net_fdm_version = 24_u32;
     let millis = time::Duration::from_millis(1000);
 
     let latitude: f64 = 45.59823;
@@ -132,93 +101,79 @@ fn main()
     let altitude: f64 = 150.0;
 
     let mut roll: f32 = 0.0;
-    let mut pitch: f32 = 0.0;
-    let mut yaw: f32 = 0.0;
+    let pitch: f32 = 0.0;
+    let yaw: f32 = 0.0;
 
     let visibility: f32 = 5000.0;
 
-    while(true)
+    loop
     {
-
         thread::sleep(millis);
 
-    //create fdm instance
-    //let fdm = FGNetFDM{altitude: 150.0, ..Default::default() }; 
-    let mut fdm: FGNetFDM = Default::default();
+        //create fdm instance
+        let mut fdm: FGNetFDM = Default::default();
 
+        //convert to network byte order
+        fdm.version = u32::from_be_bytes(fg_net_fdm_version.to_ne_bytes());
+        fdm.latitude = f64::from_be_bytes((latitude * d2r).to_ne_bytes());
+        fdm.longitude = f64::from_be_bytes((longitude * d2r).to_ne_bytes());
+        fdm.altitude = f64::from_be_bytes((altitude).to_ne_bytes());
 
-    //fdm.version = htonl(FG_NET_FDM_VERSION); //htonl is doing the same as below
-    fdm.version = u32::from_be_bytes(FG_NET_FDM_VERSION.to_ne_bytes());
-   
-    fdm.latitude = f64::from_be_bytes((latitude * D2R).to_ne_bytes());
-    fdm.longitude = f64::from_be_bytes((longitude * D2R).to_ne_bytes());
-    fdm.altitude = f64::from_be_bytes((altitude).to_ne_bytes());
+        //casting to make everything happy and then convert to network byte order
+        let _phi = roll*d2r as f32;
+        let _theta = pitch*d2r as f32;
+        let _psi = yaw*d2r as f32;
+        fdm.phi = f32::from_be_bytes(_phi.to_ne_bytes());
+        fdm.theta = f32::from_be_bytes(_theta.to_ne_bytes());
+        fdm.psi = f32::from_be_bytes(_psi.to_ne_bytes());
 
+        //convert to network byte order
+        fdm.num_engines = u32::from_be_bytes(1_u32.to_ne_bytes());
+        fdm.num_tanks = u32::from_be_bytes(1_u32.to_ne_bytes());
+        fdm.num_wheels = u32::from_be_bytes(1_u32.to_ne_bytes());
+        fdm.warp = f32::from_be_bytes(1_f32.to_ne_bytes());
+        fdm.visibility = f32::from_be_bytes(visibility.to_ne_bytes());
 
-    //casting to make everything happy
-    let _phi = roll*D2R as f32;
-    let _theta = pitch*D2R as f32;
-    let _psi = yaw*D2R as f32;
-    fdm.phi = f32::from_be_bytes(_phi.to_ne_bytes());
-    fdm.theta = f32::from_be_bytes(_theta.to_ne_bytes());
-    fdm.psi = f32::from_be_bytes(_psi.to_ne_bytes());
+        //create socket and connect to flightgear
+        let socket = UdpSocket::bind("127.0.0.1:1337").expect("couldn't bind to address");
+        socket.connect("127.0.0.1:5500").expect("connect function failed");
 
+        //convert struct array of u8 of bytes
+        let bytes: &[u8] = unsafe { any_as_u8_slice(&fdm) };
+        println!("{:?}", bytes);
 
-    fdm.num_engines = u32::from_be_bytes(1_u32.to_ne_bytes());// fdm.num_engines = htonl(1);
+        //finally send &[u8] of bytes to flight gear
+        socket.send(bytes).expect("couldn't send message");
 
-    fdm.num_tanks = u32::from_be_bytes(1_u32.to_ne_bytes());// fdm.num_tanks = htonl(1);
-    //fdm.fuel_quantity[0] = htonl(100.0);
-
-    fdm.num_wheels = u32::from_be_bytes(1_u32.to_ne_bytes());//fdm.num_wheels = htonl(1);
-
-    //fdm.cur_time = htonl(SystemTime::now());
-    fdm.warp = f32::from_be_bytes(1_f32.to_ne_bytes());// fdm.warp = htonl(1);
-
-    fdm.visibility = f32::from_be_bytes(visibility.to_ne_bytes());//fdm.visibility = htonl(visibility); //this doesnt actually seem to do anything
-
-
-    //create socket and connect to flightgear
-    let socket = UdpSocket::bind("127.0.0.1:1337").expect("couldn't bind to address");
-    socket.connect("127.0.0.1:5500").expect("connect function failed");
-
-    //convert struct array of u8
-    let bytes: &[u8] = unsafe { any_as_u8_slice(&fdm) };
-    println!("{:?}", bytes);
-
-    //finally send &[u8] of bytes to flight gear
-     socket.send(bytes).expect("couldn't send message");
-
-    roll = roll + 5.0; 
-    if roll > 20.0
-    {
-        roll = 0.0;
-    }
-    println!("Roll: {}", roll);
-
-
+        //roll 5 degrees
+        roll = roll + 5.0; 
+        if roll > 20.0
+        {
+            roll = 0.0;
+        }
+        println!("Roll: {}", roll);
     }
 }
 
-
+//for converting to slice of u8
 unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8]
 {
     ::std::slice::from_raw_parts((p as *const T) as *const u8,::std::mem::size_of::<T>(),)
 }
 
+//Some references used:
 
+    //converting to bytes
+    //https://stackoverflow.com/questions/29445026/converting-number-primitives-i32-f64-etc-to-byte-representations
 
-//Some references:
+    //htonl function in rust
+    //https://docs.rs/socket/0.0.7/socket/fn.htonl.html
 
-//converting to bytes
-//https://stackoverflow.com/questions/29445026/converting-number-primitives-i32-f64-etc-to-byte-representations
+    //struct padding in rust vs c++
+    //https://rust-lang.github.io/unsafe-code-guidelines/layout/structs-and-tuples.html
 
-//htonl function in rust
-//https://docs.rs/socket/0.0.7/socket/fn.htonl.html
+    //sending struct as u8 slice
+    //https://stackoverflow.com/questions/29307474/how-can-i-convert-a-buffer-of-a-slice-of-bytes-u8-to-an-integer
+    //https://stackoverflow.com/questions/28127165/how-to-convert-struct-to-u8
 
-//struct padding in rust vs c++
-//https://rust-lang.github.io/unsafe-code-guidelines/layout/structs-and-tuples.html
-
-//sending struct as u8 slice
-//https://stackoverflow.com/questions/29307474/how-can-i-convert-a-buffer-of-a-slice-of-bytes-u8-to-an-integer
-//https://stackoverflow.com/questions/28127165/how-to-convert-struct-to-u8
 
