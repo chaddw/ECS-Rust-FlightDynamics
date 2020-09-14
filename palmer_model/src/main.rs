458ac5e69c45270a9257886df1dad34e082b3340
chaddw <caw14f@my.fsu.edu>
2020-09-13 23:31:48 -0400

    fixed bugs

--- palmer_model/src/main.rs
+++ palmer_model/src/main.rs
@@ -10,7 +10,13 @@ struct Plane
 
     numEqns: usize, //int
     s: f64,
-    q: [f64;6],
+
+  //  q: [f64; 6],
+    q: Vec<f64>,
+    // dq1: Vec<f64>,
+    // dq2: Vec<f64>,
+    // dq3: Vec<f64>,
+    // dq4: Vec<f64>,
  
     bank: f64,
     alpha: f64,//  angle of attack
@@ -39,10 +45,10 @@ struct Plane
 //initialize plane and solves for the plane motion with range-kutta
 fn main()
 {
-    let mut x: f64;
-    let mut z: f64;
-    let mut v: f64;
-    let mut time: f64;
+    let mut x: f64 = 0.0;
+    let mut z: f64 = 0.0;
+    let mut v: f64 = 0.0;
+    let mut time: f64 = 0.0;
     let dt = 0.5; 
 
     //create plane, set airplane data
@@ -69,7 +75,12 @@ fn main()
     flap: String::from("0"),    //  Flap setting
     numEqns: 6, 
     s: 0.0,                     //  time
-    q: [0.0; 6]                 //  vx, x, vy, y, vz, z
+   // q: [0.0;6]
+     q: vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0]                //  vx, x, vy, y, vz, z
+    // dq1: vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0] 
+    // dq2: vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0] 
+    // dq31: vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0] 
+    // dq4: vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0] 
     };
 
 
@@ -102,33 +113,35 @@ impl Plane
 {
 
 
-    fn planeRungeKutta4(&mut self, ds: f64)
+    fn planeRungeKutta4(&mut self, mut ds: f64)
     {
 
-       // let mut q: [f64; 6] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
-        let mut dq1: [f64; 6] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
-        let mut dq2: [f64; 6] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
-        let mut dq3: [f64; 6] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
-        let mut dq4: [f64; 6] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+        // let mut q: [f64; 6] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+        // let mut dq1: [f64; 6] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+        // let mut dq2: [f64; 6] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+        // let mut dq3: [f64; 6] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+        // let mut dq4: [f64; 6] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+
+        let mut q = vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+        let mut qcopy = vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+        let mut dq1 = vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+        let mut dq2 = vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+        let mut dq3 = vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+        let mut dq4 = vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
 
 
-       // q = self.q; //same as doing this for loop...
-
-        // for i in 0..self.numEqns
-        // {
-        //     //q[i] = self.q[i];
-        //     println!("{}",q[i]);
-        // }
-
+        //retrieve value of dependent variable
+        q = self.q.clone();
 
         // Compute the four Runge-Kutta steps, The return 
         // value of planeRightHandSide method is an array
         // of delta-q values for each of the four steps.
 
-        self.planeRightHandSide(self.q,self.q,ds, 0.0, dq1);
-        self.planeRightHandSide(self.q, dq1, ds, 0.5, dq2);
-        self.planeRightHandSide(self.q, dq2, ds, 0.5, dq3);
-        self.planeRightHandSide(self.q, dq3, ds, 1.0, dq4);
+        self.planeRightHandSide( &mut q, &mut qcopy, &mut ds, 0.0, &mut dq1);
+        self.planeRightHandSide( &mut q, &mut dq1,   &mut ds, 0.5, &mut dq2);
+        self.planeRightHandSide( &mut q, &mut dq2,   &mut ds, 0.5, &mut dq3);
+        self.planeRightHandSide( &mut q, &mut dq3,   &mut ds, 1.0, &mut dq4);
+
 
         //  Update the dependent and independent variable values
         //  at the new dependent variable location and store the
@@ -137,11 +150,9 @@ impl Plane
 
         for i in 0..self.numEqns
         {
-            self.q[i] = self.q[i] + (dq1[i] + 2.0 * dq2[i] + 2.0 * dq3[i] + dq4[i]) / 6.0;
-            self.q[i] = self.q[i];
-
+            q[i] = q[i] + (dq1[i] + 2.0 * dq2[i] + 2.0 * dq3[i] + dq4[i]) / 6.0;
+            self.q[i] = q[i];
         }
-
 
     }
 
@@ -149,207 +160,162 @@ impl Plane
 //*************************************************************
 //  This method loads the right-hand sides for the plane ODEs
 //*************************************************************
-    fn planeRightHandSide(&mut self, q: [f64;6], deltaQ: [f64;6], ds: f64, qScale: f64, mut dq: [f64;6])
+    fn planeRightHandSide(&mut self, q: &mut Vec<f64>, deltaQ: &mut Vec<f64>, &mut ds: &mut f64, qScale:  f64, mut dq: &mut Vec<f64>)
     {
+
     //  q[0] = vx = dxdt
     //  q[1] = x
     //  q[2] = vy = dydt
     //  q[3] = y
     //  q[4] = vz = dzdt
     //  q[5] = z
-        let mut newQ: [f64;6] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]; // intermediate dependent variable values 
+        let mut newQ = vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0]; //[f64; 6] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]; // intermediate dependent variable values 
 
         let yo = -1.0_f64;
         let pi = yo.acos();
 
         let G: f64 = -9.81;
     
-        let vx: f64;
-        let vy: f64;
-        let vz: f64;
-        let x: f64;
-        let y: f64;
-        let z: f64;
-        let vh: f64;
-        let vtotal: f64;
-        let temperature: f64;
-        let grp: f64;
-        let pressure: f64;
-        let density: f64;
-        let omega: f64;
-        let factor: f64;
-        let advanceRatio: f64;
-        let thrust: f64;
-        let mut cl: f64;
-        let lift: f64;
-        let aspectRatio: f64;
-        let cd: f64;
-        let drag: f64;
-        let cosW: f64;   //  bank angle
-        let sinW: f64;   //  bank angle
-        let cosP: f64;   //  climb angle
-        let sinP: f64;   //  climb angle
-        let cosT: f64;   //  heading angle
-        let sinT: f64;   //  heading angle
-        let Fx: f64;
-        let Fy: f64;
-        let mut Fz: f64;
+        let mut cl: f64 = 0.0;
+
+        let mut cosP: f64 = 0.0;   //  climb angle
+        let mut sinP: f64= 0.0;   //  climb angle
+        let mut cosT: f64 = 0.0;   //  heading angle
+        let mut sinT: f64 = 0.0;   //  heading angle
        
+        //  Convert bank angle from degrees to radians
+        //  Angle of attack is not converted because the
+        //  Cl-alpha curve is defined in terms of degrees.
+        self.bank = self.bank.to_radians();
 
-        // let mut bank: f64;
-        // let alpha: f64;  
-        // let throttle: f64;
-        // let wingArea: f64;
-        // let wingSpan: f64;
-        // let tailArea: f64;
-        // let clSlope0: f64;    
-        // let cl0: f64;       
-        // let clSlope1: f64;    
-        // let cl1: f64;         
-        // let alphaClMax: f64;  
-        // let cdp: f64;         
-        // let eff: f64;         
-        // let mass: f64;
-        // let enginePower: f64;
-        // let engineRps: f64;   
-        // let propDiameter: f64;
-        // let a: f64;          
-        // let b: f64;          
+        //  Compute the intermediate values of the 
+        //  dependent variables.
 
-    //  Convert bank angle from degrees to radians
-    //  Angle of attack is not converted because the
-    //  Cl-alpha curve is defined in terms of degrees.
-    self.bank = self.bank.to_radians();
-   // println!("{}", self.bank);
+        for i in 0..6
+        {
+            newQ[i] = q[i] + qScale * deltaQ[i]; 
+        }
 
-    //  Compute the intermediate values of the 
-    //  dependent variables.
+        //  Assign convenenience variables to the intermediate 
+        //  values of the locations and velocities.
+        let vx: f64 = newQ[0];
+        let vy: f64 = newQ[2];
+        let vz: f64 = newQ[4];
+        let x: f64 = newQ[1];
+        let y: f64 = newQ[3];
+        let z: f64 = newQ[5];
+        let vh: f64 = (vx * vx + vy * vy).sqrt();
+        let vtotal: f64 = (vx * vx + vy * vy + vz * vz).sqrt();
 
-    for i in 0..6
-    {
-        newQ[i] = q[i] + qScale * deltaQ[i]; 
-        //println!("{}", newQ[i]);
-    }
+        //  Compute the air density
+        let temperature: f64 = 288.15 - 0.0065 * z;
+        let grp: f64 = 1.0 - 0.0065 * z / 288.15;
+        let pressure: f64 = 101325.0 * (grp.powf(5.25));
+        let density: f64 = 0.00348 * pressure / temperature;
 
-    //  Assign convenenience variables to the intermediate 
-    //  values of the locations and velocities.
-    vx = newQ[0];
-    vy = newQ[2];
-    vz = newQ[4];
-    x = newQ[1];
-    y = newQ[3];
-    z = newQ[5];
-    vh = (vx * vx + vy * vy).sqrt();
-    vtotal = (vx * vx + vy * vy + vz * vz).sqrt();
+        //  Compute power drop-off factor
+        let omega: f64 = density / 1.225;
+        let factor: f64 = (omega - 0.12)/  0.88;
 
-    //  Compute the air density
-    temperature = 288.15 - 0.0065 * z;
-    grp = 1.0 - 0.0065 * z / 288.15;
-    pressure = 101325.0 * (grp.powf(5.25));
-    density = 0.00348 * pressure / temperature;
+        //  Compute thrust
+        let advanceRatio: f64 = vtotal / (self.engineRps * self.propDiameter);
+        let thrust: f64 = self.throttle * factor * self.enginePower * (self.a + self.b * advanceRatio * advanceRatio) / (self.engineRps * self.propDiameter);
 
-    //  Compute power drop-off factor
-    omega = density / 1.225;
-    factor = (omega - 0.12)/  0.88;
+        //  Compute lift coefficient. The Cl curve is 
+        //  modeled using two straight lines.
+        if  self.alpha < self.alphaClMax
+        {
+            cl = self.clSlope0 * self.alpha + self.cl0;
+        }
+        else 
+        {
+            cl = self.clSlope1 * self.alpha + self.cl1;
+        }
 
-    //  Compute thrust
-    advanceRatio = vtotal / (self.engineRps * self.propDiameter);
-    thrust = self.throttle * factor * self.enginePower * (self.a + self.b * advanceRatio * advanceRatio) / (self.engineRps * self.propDiameter);
+        //  Include effects of flaps and ground effects.
+        //  Ground effects are present if the plane is
+        //  within 5 meters of the ground.
+        if self.flap == "20"
+        {
+            cl += 0.25;
+        }
+        if self.flap == "40"
+        {
+            cl += 0.5;
+        }
+        if z < 5.0
+        {
+            cl += 0.25;
+        }
 
-    //  Compute lift coefficient. The Cl curve is 
-    //  modeled using two straight lines.
-    if  self.alpha < self.alphaClMax
-    {
-        cl = self.clSlope0 * self.alpha + self.cl0;
-    }
-    else 
-    {
-        cl = self.clSlope1 * self.alpha + self.cl1;
-    }
+        //  Compute lift
+        let lift: f64 = 0.5 * cl * density * vtotal * vtotal * self.wingArea;
 
-    //  Include effects of flaps and ground effects.
-    //  Ground effects are present if the plane is
-    //  within 5 meters of the ground.
-    if self.flap == "20"
-    {
-        cl += 0.25;
-    }
-    if self.flap == "40"
-    {
-        cl += 0.5;
-    }
-    if z < 5.0
-    {
-        cl += 0.25;
-    }
-
-    //  Compute lift
-    lift = 0.5 * cl * density * vtotal * vtotal * self.wingArea;
-
-    // //  Compute drag coefficient
-    aspectRatio = self.wingSpan * self.wingSpan / self.wingArea;
-    cd = self.cdp + cl * cl / (pi * aspectRatio * self.eff);
-    
-    // //  Compute drag force
-    drag = 0.5 * cd * density * vtotal * vtotal * self.wingArea;
+        // //  Compute drag coefficient
+        let aspectRatio: f64 = self.wingSpan * self.wingSpan / self.wingArea;
+        let cd = self.cdp + cl * cl / (pi * aspectRatio * self.eff);
+        
+        // //  Compute drag force
+        let drag: f64 = 0.5 * cd * density * vtotal * vtotal * self.wingArea;
 
 
-    //  Define some shorthand convenience variables
-    //  for use with the rotation matrix.
-    //  Compute the sine and cosines of the climb angle,
-    //  bank angle, and heading angle;
-    cosW = self.bank.cos(); 
-    sinW = self.bank.sin(); 
+        //  Define some shorthand convenience variables
+        //  for use with the rotation matrix.
+        //  Compute the sine and cosines of the climb angle,
+        //  bank angle, and heading angle;
+        let cosW: f64 = self.bank.cos(); 
+        let sinW: f64 = self.bank.sin(); 
 
-    if  vtotal == 0.0
-    {
-        cosP = 1.0;
-        sinP = 0.0;
-    }
-    else
-    {
-        cosP = vh / vtotal;  
-        sinP = vz / vtotal;  
-    }
-    
-    if vh == 0.0
-    {
-        cosT = 1.0;
-        sinT = 0.0;
-    }
-    else
-    {
-        cosT = vx / vh;
-        sinT = vy / vh;
-    }
+        if  vtotal == 0.0
+        {
+            cosP = 1.0;
+            sinP = 0.0;
+        }
+        else
+        {
+            cosP = vh / vtotal;  
+            sinP = vz / vtotal;  
+        }
+        
+        if vh == 0.0
+        {
+            cosT = 1.0;
+            sinT = 0.0;
+        }
+        else
+        {
+            cosT = vx / vh;
+            sinT = vy / vh;
+        }
 
 
-    //  Convert the thrust, drag, and lift forces into
-    //  x-, y-, and z-components using the rotation matrix.
-    Fx = cosT * cosP * (thrust - drag) + (sinT * sinW - cosT * sinP * cosW) * lift;
-    Fy = sinT * cosP * (thrust - drag) + (-cosT * sinW - sinT * sinP * cosW) * lift;
-    Fz = sinP * (thrust - drag) + cosP * cosW * lift;
+        //  Convert the thrust, drag, and lift forces into
+        //  x-, y-, and z-components using the rotation matrix.
+        let Fx: f64 = cosT * cosP * (thrust - drag) + (sinT * sinW - cosT * sinP * cosW) * lift;
+        let Fy: f64 = sinT * cosP * (thrust - drag) + (-cosT * sinW - sinT * sinP * cosW) * lift;
+        let mut Fz: f64 = sinP * (thrust - drag) + cosP * cosW * lift;
 
-    //  Add the gravity force to the z-direction force.
-    Fz = Fz + self.mass * G;
+        //  Add the gravity force to the z-direction force.
+        Fz = Fz + self.mass * G;
 
-    //  Since the plane can't sink into the ground, if the
-    //  altitude is less than or equal to zero and the z-component
-    //  of force is less than zero, set the z-force
-    //  to be zero.
-    if  z <= 0.0 && Fz <= 0.0  
-    {
-        Fz = 0.0;
-    }
+        //  Since the plane can't sink into the ground, if the
+        //  altitude is less than or equal to zero and the z-component
+        //  of force is less than zero, set the z-force
+        //  to be zero.
+        if  z <= 0.0 && Fz <= 0.0  
+        {
+            Fz = 0.0;
+        }
 
-    //  Load the right-hand sides of the ODE's
-    dq[0] = ds * (Fx / self.mass);
-    dq[1] = ds * vx;
-    dq[2] = ds * (Fy / self.mass);
-    dq[3] = ds * vy;
-    dq[4] = ds * (Fz / self.mass);
-    dq[5] = ds * vz;
+        //  Load the right-hand sides of the ODE's
+        dq[0] = ds * (Fx / self.mass);
+        dq[1] = ds * vx;
+        dq[2] = ds * (Fy / self.mass);
+        dq[3] = ds * vy;
+        dq[4] = ds * (Fz / self.mass);
+        dq[5] = ds * vz;
 
+ 
     }
 
 }
