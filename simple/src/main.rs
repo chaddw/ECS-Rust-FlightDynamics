636378ddf0e26a2b000c05524f796959a1a633d2
chaddw <caw14f@my.fsu.edu>
2020-11-19 19:24:57 -0500

    deleting some old stuff

--- simple/src/main.rs
+++
@@ -1,7 +0,0 @@
-use std::net::UdpSocket;
-
-fn main() {
-    let socket = UdpSocket::bind("127.0.0.1:1337").expect("couldn't bind to address");
-    socket.connect("127.0.0.1:5500").expect("connect function failed");
-    socket.send(&[0, 1, 2]).expect("couldn't send message");
-}
